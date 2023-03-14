92e35a16cd6f57addb1bbf644b853c35ad512e91
shweta.jivtode <shweta.jivtode@NAGSEZ545633L.ad.infosys.com>
2023-03-14 19:23:51 +0530

    d

--- test/src/test/my.java
+++ test/src/test/my.java
@@ -4,8 +4,12 @@ public class my {
 
 	public static void main(String args[])
 	{
-	System.out.println("Hello World"); //Prints on screen “Hello World”
+	System.out.println("Hello World");//Prints on screen “Hello World”
+	System.out.println("Hello meena");
+	System.out.println("Hello teena");
+	
 	}
+	
 	
 	
 	
